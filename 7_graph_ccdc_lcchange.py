42411ba363db9b448afd3455f0e1b27445021a0c
dzelenak-usgs <daniel.zelenak.ctr@usgs.gov>
2017-09-27 12:34:04 -0500

    fixed to search for files in subfolders

--- 7_graph_ccdc_lcchange.py
+++ 7_graph_ccdc_lcchange.py
@@ -9,30 +9,42 @@ for specific years.
 
 """
 
-import os, sys, glob, pprint
-from osgeo import gdal
-import numpy as np
+import os
+import sys
+import argparse
+
 import matplotlib
+import numpy as np
+from osgeo import gdal
+
 matplotlib.use("agg")
 import matplotlib.pyplot as plt
 from pandas import DataFrame
 
-#%%
-def get_rasters(indir):
 
-    in_cl = glob.glob(indir + os.sep + "*.tif")
+def get_rasters(indir):
+    in_cl = []
+
+    # in_cl = glob.glob(indir + os.sep + "*.tif")
+
+    for root, folders, files in os.walk(indir):
+
+        for folder in folders:
+
+            for file in files:
+
+                if file[-4:] == ".tif":
+                    in_cl.append(os.path.join(root, folder) + os.sep + file)
 
     if in_cl is None:
-    
         print("\n**Could not locate input LC Change files**\n")
-        
+
         sys.exit(1)
-    
-    return in_cl # , in_ct
 
-#%%
+    return in_cl
+
+
 def read_data(cl):
-
     """
     Purpose: Open the Trends from-to LC Change .tif file.  
                 Iterate through the list of from-to classes.
@@ -47,7 +59,7 @@ def read_data(cl):
         masked_sum = list, the total number of pixels for each class
         total_pixels = the total number of Trends pixels in the tile             
     """
-    
+
     cl_src = gdal.Open(cl, gdal.GA_ReadOnly)
 
     cl_data = cl_src.GetRasterBand(1).ReadAsArray()
@@ -56,46 +68,44 @@ def read_data(cl):
     classes = [i for i in range(0, 10)]
 
     # list of all possible from/to combinations
-    classmix = [str(i) + str(j) for i in classes for j in classes]    
+    classmix = [str(i) + str(j) for i in classes for j in classes]
 
     # classes = np.unique(cl_data)
-    
+
     masked_sum = []
 
     for ind, c in enumerate(classmix):
-        
+
         mask_cl = np.copy(cl_data)
-        
+
         if ind == 0:
-            
+
             mask_cl[mask_cl != int(c)] = 111
             mask_cl[mask_cl == int(c)] = 1
             mask_cl[mask_cl == 111] = 0
 
         else:
-        
+
             mask_cl[mask_cl != int(c)] = 0
             mask_cl[mask_cl == int(c)] = 1
-        
+
         holder = np.sum(mask_cl)
-      
+
         masked_sum.append(holder)
-        
+
         # gives an idea of progress for the user
         print(c, " ", holder)
-    
+
     cl_data, cl_src, mask_cl = None, None, None
 
     return classmix, masked_sum
 
-#%%
+
 def get_figure(label_set, df, tile, year1, year2, outname):
-    
     """Purpose: Generate a matplotlib figure of n rows and 2 columns, the number
                 rows is equal to the number of classes (label_set).  Column 1
-                will contain vertical bar charts colored by the 'from' class.
-                Column 2 will contain tables showing the count and percent for
-                row n 'from' class.
+                will contain vertical bar charts. Column 2 will contain tables
+                showing the count and percent for row n 'from' class.
     Args:
         label_set = list, list of classes
         df = pandas DataFrame object contains class names, counts and percents
@@ -105,7 +115,7 @@ def get_figure(label_set, df, tile, year1, year2, outname):
     Returns:
         None
     """
-    
+
     # RGB colors taken from Arc colormap and rescaled from 0-255 to 0-1
     """
     colors = {"0" : (0.0, 0.0, 0.0),
@@ -121,30 +131,29 @@ def get_figure(label_set, df, tile, year1, year2, outname):
     """
 
     # Generate figure with length(label_set) rows and 2 columns
-    fig, axes = plt.subplots(nrows = len(label_set), ncols = 2, 
-                                  figsize=(16, 50))
-    
+    fig, axes = plt.subplots(nrows=len(label_set), ncols=2,
+                             figsize=(16, 50))
+
     # Add figure title
-    fig.text(0.5, .90, "%s CCDC %s to %s From-To Classes" % (tile, year1, year2), 
-                  horizontalalignment="center", fontsize=22, fontweight='bold')
-    
+    fig.text(0.5, .90, "%s CCDC %s to %s From-To Classes" % (tile, year1, year2),
+             horizontalalignment="center", fontsize=22, fontweight='bold')
+
     print(label_set)
     for i, L in enumerate(label_set):
-        
+
         t = []
-        
+
         # iterate through rows of dataframe to retrieve values for class L
         for x in df.itertuples():
-            
-            if x[1][0] == L :
-            
+
+            if x[1][0] == L:
                 t.append(x[1:])
-            
+
         df_temp = DataFrame(t)
-        
+
         # Assign column names
         df_temp.columns = ["Name", "Count", "Percent of Tile"]
-        
+
         # generate bar charts in first column for class L in row i
         axes[i, 0].bar(df_temp.index, df_temp.Count, width=0.8)
         axes[i, 0].set_title('"From" Class ' + L + " Bar Chart")
@@ -152,116 +161,92 @@ def get_figure(label_set, df, tile, year1, year2, outname):
         axes[i, 0].set_xticklabels(df_temp.Name)
         axes[i, 0].set_xlabel("Class")
         axes[i, 0].set_ylabel("Count")
-        
+
         # generate tables in second column for class L in row i
-        axes[i, 1].table(cellText=df_temp.values, bbox=[0,0,1,1], colLabels=df_temp.columns)        
+        axes[i, 1].table(cellText=df_temp.values, bbox=[0, 0, 1, 1], colLabels=df_temp.columns)
         axes[i, 1].set_title('"From" Class ' + L + " Table")
         axes[i, 1].set_xticks([])
         axes[i, 1].set_yticks([])
-    
+
     # save the figure as a .png file
     plt.savefig(outname, bbox_inches="tight", dpi=150)
-    
+
     return None
 
-#%%
+
 def usage():
     print("\n\t[-i the full path to the input raster file]\n"
-    "\t[-o the full path to the output graph image (.png)]\n"
-    "\t[-tile the tile name (used for graph title)]\n"
-    "\t[-help display this message]\n")
+          "\t[-o the full path to the output graph image (.png)]\n"
+          "\t[-tile the tile name (used for graph title)]\n"
+          "\t[-help display this message]\n")
 
     print("Example: python graph_ccdc_lcchange.py -i C:\... -o C:\... -tile h05v02 "
           "-frm 1992 -to 2011")
 
-#%%
+
 def main():
+    parser = argparse.ArgumentParser()
 
-    argv = sys.argv
+    parser.add_argument("-i", "--input", type=str, required=True,
+                        help="Full path to the directory containing all PyCCD LC Change products")
 
-    if len(argv) <= 1:
-        
-        print("***Missing required arguments***\n")
-        
-        print("Try using -help\n")
-        
-        sys.exit(0)
+    parser.add_argument("-o", "--output", type=str, required=True,
+                        help="Full path to the output folder where the graphs will be saved")
 
-    i = 1
-    
-    while i < len(argv):
-        
-        arg = argv[i]
+    parser.add_argument("-t", "-tile", "--tile", type=str, required=True,
+                        help="The name of the ARD tile.  This is only used for the graph title")
 
-        if arg == "-i":
-            i = i + 1
-            in_dir = argv[i]
-        
-        elif arg == "-o":
-            i = i + 1
-            out_dir = argv[i]
-        
-        elif arg == "-tile":
-            i = i + 1
-            tile = argv[i]
-        
-        elif arg == "-help":
-            i = i + 1
-            usage()
-            sys.exit(1)
-        
-        i += 1
-    
-    if not os.path.exists(out_dir): 
-        
+    args = parser.parse_args()
+
+    out_dir = args.output
+
+    in_dir = args.input
+
+    tile = args.tile
+
+    if not os.path.exists(out_dir):
         os.makedirs(out_dir)
 
     in_files = get_rasters(in_dir)
-    
-    for in_cl in in_files:    
 
+    for in_cl in in_files:
         year1 = os.path.basename(in_cl)[-16:-12]
-    
-        year2 = os.path.basename(in_cl)[-10:-6]
-        
-        outname = "%s%s%s_ccdc%sto%s_lchange.png" % \
-        (out_dir, os.sep, tile, year1, year2)
 
+        year2 = os.path.basename(in_cl)[-10:-6]
+
+        outname = "%s%s%s_ccdc%sto%s_lchange.png" % (out_dir, os.sep, tile, year1, year2)
 
         classes, class_sums = read_data(in_cl)
-    
+
         # calculate the percent of the total for each from-to class
-        class_perc= ["%.2f" % (val / 25000000.0 * 100.0) for val in class_sums]
-    
+        class_perc = ["%.2f" % (val / 25000000.0 * 100.0) for val in class_sums]
+
         # convert the items in classes list to strings and save in a new list
         labels = [str(c) for c in classes]
-    
+
         # get a set of the unique "from" classes (the first digit)
-        labels_ =  [l[0] for l in labels]
-        label_set = set(labels_) # converting to set removes duplicates
-        label_set = list(label_set) # convert back to list to allow indexing
-        
+        labels_ = [l[0] for l in labels]
+        label_set = set(labels_)  # converting to set removes duplicates
+        label_set = list(label_set)  # convert back to list to allow indexing
+
         # Cluttered way to return a list of class values with the correct order
         label_set = [int(l) for l in label_set]
         label_set.sort()
         label_set = [str(l) for l in label_set]
-    
+
         # create list of tuples to populate three data columns
-        data = [(x,y,z) for x,y,z in zip(labels, class_sums, class_perc)]
-            
+        data = [(x, y, z) for x, y, z in zip(labels, class_sums, class_perc)]
+
         # create pandas dataframe from the list of tuples
         df = DataFrame(data)
-        
+
         # add column names to the dataframe
         df.columns = ["Name", "Count", "Percent"]
-    
+
         get_figure(label_set, df, tile, year1, year2, outname)
-    
+
     return None
 
-#%%
+
 if __name__ == "__main__":
-    
     main()
-
-
