2de4e098916d116decddc73d1dd1e186514d1bff
dzelenak-usgs <daniel.zelenak.ctr@usgs.gov>
2017-10-03 08:26:39 -0500

    added argparse, minor PEP fixes

--- 5_ccdc_yofc.py
+++ 5_ccdc_yofc.py
@@ -1,35 +1,35 @@
 #!usr/bin/env python
 # -*- coding: utf-8 -*-
 
-'''
+"""
 Author: Dan Zelenak
 Last Updated: 5/12/2017
 Description:  Go through ChangeMap layers and flag each pixel with the 
 first year in which a change is detected.  The resulting raster will show 
 the first year that change occurred for each pixel.
-'''
+"""
 
-import os, sys, datetime, glob, subprocess, re
-
+import datetime
+import glob
 import numpy as np
+import os
+import re
+import subprocess
+import sys
+import argparse
 
-print (sys.version)
+from osgeo import gdal
 
-try:
-    from osgeo import gdal
-    # from osgeo.gdalconst import *
-except ImportError:
-    import gdal
+print(sys.version)
 
 t1 = datetime.datetime.now()
-print ("Processing started at: ", t1.strftime("%Y-%m-%d %H:%M:%S\n"))
+print("Processing started at: ", t1.strftime("%Y-%m-%d %H:%M:%S\n"))
 
 gdal.AllRegister()
 gdal.UseExceptions()
 
-#%%
-def get_infiles(infolder, y1, y2):
 
+def get_infiles(infolder, y1, y2):
     """Generate a list of the input change map layers with full paths
 
     Args:
@@ -43,60 +43,53 @@ def get_infiles(infolder, y1, y2):
         rlist = the clipped list of change map raster files based on y1, y2
     """
 
-    templist = glob.glob("{a}{b}ChangeMap*.tif".format( a=infolder, b=os.sep ))
+    templist = glob.glob("{a}{b}ChangeMap*.tif".format(a=infolder, b=os.sep))
 
     templist.sort(reverse=False)
 
-    if y1==None or y2==None:
+    if y1 is None or y2 is None:
 
         return templist
 
     else:
 
-        ylist = [y for y in range(int(y1), int(y2)+1)]
+        ylist = [y for y in range(int(y1), int(y2) + 1)]
 
         rlist = [r for y in ylist for r in templist if str(y) in r]
-        
+
         rlist.sort(reverse=False)
 
         return rlist
 
-#%%
-def do_calc(in_f, temp_f):
 
+def do_calc(in_f, temp_f):
     """
     """
     src = gdal.Open(in_f, gdal.GA_ReadOnly)
 
     srcdata = src.GetRasterBand(1).ReadAsArray()
 
-    #temp_out = srcdata
-
-    #temp_out[temp_f == 0] = int(re.split("[_ .]", in_f)[1])
-
     root, fname = os.path.split(in_f)
-    
+
     year_value = int(re.split("[_ .]", fname)[1])
-    
-    # if temp_f == None:
+
     if not np.any(temp_f):
-        
+
         srcdata_holder = np.copy(srcdata)
-        
-        srcdata_holder[ srcdata_holder != 0] = year_value
+
+        srcdata_holder[srcdata_holder != 0] = year_value
 
         src = None
 
         return srcdata_holder
 
-    # elif temp_f != None:
     elif np.any(temp_f):
 
         srcdata_holder = np.copy(srcdata)
 
-        srcdata_holder[ temp_f != 0 ] = 0
+        srcdata_holder[temp_f != 0] = 0
 
-        srcdata_holder[ srcdata_holder != 0 ] = year_value
+        srcdata_holder[srcdata_holder != 0] = year_value
 
         srcdata = (srcdata * 0) + temp_f + srcdata_holder
 
@@ -104,18 +97,16 @@ def do_calc(in_f, temp_f):
 
         return srcdata
 
-#%%
-def get_outfile(inlist, from_y, to_y, outdir):
 
+def get_outfile(inlist, from_y, to_y, outdir):
     """
     """
 
     years = []
 
-    if from_y == None:
+    if from_y is None:
 
         for r in inlist:
-
             r = os.path.basename(r)
 
             years.append(re.split("[_ .]", r)[1])
@@ -124,15 +115,14 @@ def get_outfile(inlist, from_y, to_y, outdir):
 
         from_y, to_y = years[0], years[-1]
 
-    outname = "ccdc{a}to{b}yofc.tif".format( a=from_y[-2:], b=to_y[-2:] )
+    outname = "ccdc{a}to{b}yofc.tif".format(a=from_y[-2:], b=to_y[-2:])
 
     outfile = outdir + os.sep + outname
 
     return outfile
 
-#%%
-def get_raster(infile, indata, out_r):
 
+def get_raster(infile, indata, out_r):
     """
     """
     in_src = gdal.Open(infile, gdal.GA_ReadOnly)
@@ -141,34 +131,30 @@ def get_raster(infile, indata, out_r):
     rows = in_src.RasterYSize
 
     driver = gdal.GetDriverByName("GTiff")
-    
+
     outfile = driver.Create(out_r, cols, rows, 1, gdal.GDT_UInt16)
-    
+
     if outfile is None:
-            
-        print ("\nCould not create image file {a}".format
-                   ( a=os.path.basename(out_r) ))
-            
+        print("\nCould not create image file {a}".format
+              (a=os.path.basename(out_r)))
+
         sys.exit(1)
-            
+
     outband = outfile.GetRasterBand(1)
     outband.WriteArray(indata, 0, 0)
-        
+
     outband.FlushCache()
     outband.SetNoDataValue(32767)
-        
-    outfile.SetGeoTransform( in_src.GetGeoTransform() )
-    outfile.SetProjection( in_src.GetProjection() )
-    
+
+    outfile.SetGeoTransform(in_src.GetGeoTransform())
+    outfile.SetProjection(in_src.GetProjection())
+
     in_src, outfile = None, None
 
     return None
 
 
-
-#%%
 def add_color_table(in_vrt, clr_table, dtype):
-    
     """Write color map info to a VRT file
     
     Args:
@@ -178,8 +164,7 @@ def add_color_table(in_vrt, clr_table, dtype):
     Return:
         out_vrt = the VRT with color map info written to it
     """
-    
-    
+
     color_table = open(clr_table, "r")
 
     (dirName, fileName) = os.path.split(in_vrt)
@@ -187,48 +172,46 @@ def add_color_table(in_vrt, clr_table, dtype):
 
     out_vrt = r"{0}{1}zzzzz{2}_temp.vrt".format(dirName, os.sep, fileBase)
 
-    in_txt = open(in_vrt, 'r+')
-    out_txt = open(out_vrt, 'w')
+    # in_txt = open(in_vrt, 'r+')
+    # out_txt = open(out_vrt, 'w')
 
     with open(in_vrt, 'r+') as in_txt, open(out_vrt, "w") as out_txt:
-        
-       # key is the line after which to insert the color table in the VRT
+
+        # key is the line after which to insert the color table in the VRT
         key = '<VRTRasterBand dataType="{0}" band="1">'.format(dtype)
-        
-       # subkey is a line that doesn't need to be in the new VRT text
+
+        # subkey is a line that doesn't need to be in the new VRT text
         subkey = "   <ColorInterp>Gray</ColorInterp>"
-        
-       # get lines in a list
+
+        # get lines in a list
         txt_read = in_txt.readlines()
 
         for line in txt_read:
-        
+
             if subkey in line:
-                
-                  continue
-            
+
+                continue
+
             else:
-                
+
                 writetxt = r"{0}".format(line)
-                
+
                 out_txt.write(writetxt)
 
                 # insert color table following keywords
                 if key in line:
-                
-                    #print "\nFound the key!\n"
+
+                    # print "\nFound the key!\n"
                     color_read = color_table.readlines()
-                    
-                    #print 'writing color table to vrt'
+
+                    # print 'writing color table to vrt'
                     for ln in color_read:
-                    
                         out_txt.write(ln)
 
     return out_vrt
 
-#%%
+
 def add_color(outdir, raster):
-    
     """Add a color map to the created raster files
     
     Args:
@@ -238,49 +221,44 @@ def add_color(outdir, raster):
     Return:
         None
     """
-    
+
     namex = os.path.basename(raster)
     name = os.path.splitext(namex)[0]
-    
+
     if not os.path.exists(outdir + os.sep + "Color"):
-        
         os.mkdir(outdir + os.sep + "Color")
-    
+
     outfile = outdir + os.sep + "Color" + os.sep + namex
-    
+
     clr_table = "color_yolc.txt"
-    
+
     outcsv_file = r'%s%szzzzzz_%s_list.csv' % (outdir, os.sep, name)
-        
+
     if os.path.isfile(outcsv_file):
-        
         os.remove(outcsv_file)
 
     with open(outcsv_file, 'wb') as outcsv2_file:
-            
         outcsv2_file.write(raster.encode('utf-8') + "\r\n".encode('utf-8'))
 
-    temp_vrt = '{}{}zzzz_{}.vrt'.format(outdir, os.sep, name)
-    com      = 'gdalbuildvrt -q -input_file_list %s %s' \
-                % (outcsv_file, temp_vrt)
+    temp_vrt = f'{outdir}{os.sep}zzzz_{name}.vrt'
+
+    com = f'gdalbuildvrt -q -input_file_list {outcsv_file} {temp_vrt}'
+
     subprocess.call(com, shell=True)
 
     out_vrt = add_color_table(temp_vrt, clr_table, 'UInt16')
-    
-    runCom  = "gdal_translate -of %s -ot UInt16 -q" \
-               " -stats -a_srs EPSG:5070 %s %s"\
-               % ("GTiff", out_vrt, outfile)
-    subprocess.call(runCom, shell=True)
+
+    run_com = f'gdal_translate -of GTiff -ot UInt16 -q -stats {out_vrt} {outfile}'
+
+    subprocess.call(run_com, shell=True)
 
     # remove the temp files used for adding the color tables
     for v in glob.glob(outdir + os.sep + "zzz*"):
-    
         os.remove(v)
-    
-    
+
     return None
 
-#%%
+
 def usage():
     print('\nUsage:python 5_ccdc_yofc.py\n\n \
     [\t-i Full path to the directory containing the CCDC annual change layers] \n\
@@ -292,87 +270,59 @@ def usage():
 
     return None
 
-#%%
+
 def main():
+    parser = argparse.ArgumentParser()
 
-    fromyear, toyear = None, None
+    parser.addargument("-i", "--input", type=str, required=True,
+                       help="Full path to the input directory containing annual CCDC DOY change layers")
 
-    argv = sys.argv
+    parser.addargument("-frm", "-from", "--year1", type=str, required=False, help="Beginning year")
 
-    if argv is None:
-        sys.exit(0)
+    parser.addargument("-to", "--year2", type=str, required=False, help="End Year")
 
-    # Parse command line arguments.
-    i = 1
-    while i < len(argv):
-        arg = argv[i]
+    parser.addargument("-o", "--output", type=str, required=True, help="Full path to the output folder")
 
-        if arg == '-i':
-            i           = i + 1
-            inputdir        = argv[i]
+    args = parser.parse_args()
 
-        elif arg == '-frm':
-            i           = i + 1
-            fromyear      = argv[i]
-
-        elif arg == '-to':
-            i           = i + 1
-            toyear      = argv[i]
-
-        elif arg == '-o':
-            i           = i + 1
-            outputdir      = argv[i]
-
-        elif arg == '-help':
-            usage()
-            sys.exit(1)
-
-        elif arg[:1] == ':':
-            print('Unrecognized command option: %s' % arg)
-            usage()
-            sys.exit(1)
-
-        i += 1
+    outputdir = args.output
+    inputdir = args.input
+    fromyear = args.year1
+    toyear = args.year2
 
     # create output directory if it doesn't already exist
-    if not os.path.exists(outputdir): os.mkdir(outputdir)
+    if not os.path.exists(outputdir):
+        os.mkdir(outputdir)
 
     # generate list of input files including their full path
     # and clip the list to the date range if one was given
     in_files = get_infiles(inputdir, fromyear, toyear)
 
-    # temp_holder1 = get_temp(in_files[0])
+    yolc_data = do_calc(in_files[0], None)
 
-    for x in range(len(in_files)):
+    temp_holder1 = np.copy(yolc_data)
 
-        print ("\nProcessing file ", os.path.basename(in_files[x]))
-        
-        if x == 0:
+    for x in range(1, len(in_files)):
+        print("\nProcessing file ", os.path.basename(in_files[x]))
 
-            yolc_data = do_calc(in_files[x], None)
+        yolc_data = do_calc(in_files[x], temp_holder1)
 
-            temp_holder1 = np.copy( yolc_data )
-
-        else:
-
-            yolc_data = do_calc(in_files[x], temp_holder1)
-
-            temp_holder1 = np.copy( yolc_data )
+        temp_holder1 = np.copy(yolc_data)
 
     out_raster = get_outfile(in_files, fromyear, toyear, outputdir)
- 
+
     get_raster(in_files[0], yolc_data, out_raster)
 
     add_color(outputdir, out_raster)
 
     return None
 
-#%%
+
 if __name__ == '__main__':
     main()
 
 t2 = datetime.datetime.now()
-print ("\nCompleted at: ", t2.strftime("%Y-%m-%d %H:%M:%S"))
+print("\nCompleted at: ", t2.strftime("%Y-%m-%d %H:%M:%S"))
 
 tt = t2 - t1
-print ("Processing time: " + str(tt),"\n")
+print("Processing time: " + str(tt), "\n")
