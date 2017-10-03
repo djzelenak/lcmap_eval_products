b33f23ed396d1e1d7ea9834f35742cfbcb46cc6f
dzelenak-usgs <daniel.zelenak.ctr@usgs.gov>
2017-10-03 10:29:44 -0500

    Lots of simplification

--- 5_ccdc_yolc.py
+++ 5_ccdc_yolc.py
@@ -1,304 +1,241 @@
-'''
-Original Script Name: Frequency_computation.py
-Author: Devendra Dahal
-Date:	8/18/2015
-Last Updated : 3/7/2017 by Dan Zelenak, 3/9/2017 by Dan Zelenak to work LCSRLNST01
+"""
+Author: Dan Zelenak
+Date: 3/7/2017
 Description:  Go through ChangeMap layers in reverse (ie start at 2016 and work back to 1985)
 and flag each pixel when the first change is detected going back through time.  The result is a single raster
-that shows in which year the last registered change occured.
-'''
+that shows in which year the last detected change occurred.
+"""
 
-import os, sys, traceback, datetime, glob, subprocess, re, pprint
+import datetime
+import glob
+import os
+import pprint
+import re
+import subprocess
+import sys
+import traceback
+import argparse
+import numpy as np
 
-print (sys.version)
+from osgeo import gdal
 
-GDALPath = '/usr/bin'
-#GDALPath = r'C:\Users\dzelenak\Workspace\Testing\LCMAP_Tools\dist'
-
-try:
-	from osgeo import gdal
-	from osgeo.gdalconst import *
-except ImportError:
-	import gdal
+print(sys.version)
 
 t1 = datetime.datetime.now()
-print ("Processing started at: ", t1.strftime("%Y-%m-%d %H:%M:%S\n"))
+print("Processing started at: ", t1.strftime("%Y-%m-%d %H:%M:%S\n"))
+
 
 def add_color_table(in_vrt, clr_table, dtype):
-	color_table = open(clr_table, 'r')
+    color_table = open(clr_table, 'r')
 
-	(dirName, fileName) = os.path.split(in_vrt)
-	(fileBase, fileExt) = os.path.splitext(fileName)
+    (dirName, fileName) = os.path.split(in_vrt)
 
-	out_vrt = r'{0}{1}zzzzz{2}_temp.vrt'.format(dirName, os.sep, fileBase)
+    (fileBase, fileExt) = os.path.splitext(fileName)
 
-	in_txt = open(in_vrt, 'r+')
-	out_txt = open(out_vrt, 'w')
+    out_vrt = r'{0}{1}zzzzz{2}_temp.vrt'.format(dirName, os.sep, fileBase)
 
-	with open(in_vrt, 'r+') as in_txt, open(out_vrt, 'w') as out_txt:
-		# key is the line after which to insert the color table in the new VRT text
-		key = '<VRTRasterBand dataType="{0}" band="1">'.format(dtype)
-		# subkey is a line that doesn't need to be in the new VRT text
-		subkey = '   <ColorInterp>Gray</ColorInterp>'
-		# get lines in a list
-		txt_read = in_txt.readlines()
+    with open(in_vrt, 'r+') as in_txt, open(out_vrt, 'w') as out_txt:
+        # key is the line after which to insert the color table in the new VRT text
+        key = '<VRTRasterBand dataType="{0}" band="1">'.format(dtype)
 
-		for line in txt_read:
-			if subkey in line:
-				#print '\nfound the subkey to ignore\n'
-				continue
-			else:
-				writetxt = r'{0}'.format(line)
-				out_txt.write(writetxt)
+        # subkey is a line that doesn't need to be in the new VRT text
+        subkey = '   <ColorInterp>Gray</ColorInterp>'
 
-				# insert color table following keywords
-				if key in line:
-					#print "\nFound the key!\n"
-					color_read = color_table.readlines()
-					#print 'writing color table to vrt'
-					for ln in color_read:
-						out_txt.write(ln)
-		#print 'done writing!'
-	return out_vrt
-	# (FFormat, outDir, rList, exten, tempoutfile)
+        # get lines in a list
+        txt_read = in_txt.readlines()
 
-def RasterChange(fformat, outdir, rasterlist, exten, outfile):
-	#create outfile raster of all 0's that matches the format of the first raster in rasterlist
-	#**stored in the outfile raster**
+        for line in txt_read:
+            if subkey in line:
 
-	runCalc0  = '%s/gdal_calc.py --format %s --type %s -A %s --outfile %s --calc="0 *(A > 0)" ' \
-	% (GDALPath, fformat, 'Int16', rasterlist[0], outfile)
-	#print 'running calculate zeros\n'
-	subprocess.call(runCalc0, shell=True)
+                # print '\nfound the subkey to ignore\n'
+                continue
+            else:
+                writetxt = r'{0}'.format(line)
 
-	for i in range(len(rasterlist)): #Changed rasterlist - 1 to rasterlist, to count last raster if change occured
-		raster1 = rasterlist[i]
+                out_txt.write(writetxt)
 
-		raster1out = outdir + os.sep + 'xxxx_raster1out' + exten
+                # insert color table following keywords
+                if key in line:
+                    # print "\nFound the key!\n"
 
-		# raster1out stores the yes/no change layer for rasterlist[i]
-		runCalc1  = '%s/gdal_calc.py --format %s --type %s -A %s --outfile %s --calc="1 *(A > 0)" ' \
-		% (GDALPath, fformat, 'Int16', raster1, raster1out)
+                    color_read = color_table.readlines()
 
-		subprocess.call(runCalc1, shell=True)
+                    # print 'writing color table to vrt'
 
-		tempraster = outdir + os.sep + 'xxxx_' + str(i) + exten
+                    for ln in color_read:
 
-		if not os.path.exists(tempraster):
-			# tempraster holds the sum of the yes/no raster1out and the outfile
-			runCalc2  = '%s/gdal_calc.py --format %s --type %s -A %s -B %s --outfile %s --calc="(A + B)" --overwrite'\
-			 % (GDALPath, fformat, 'Int16', raster1out, outfile, tempraster)
+                        out_txt.write(ln)
 
-			# outfile gets updated with the new sum
-			outfile = tempraster
-			#print '\nrunning change calculation\n'
-			subprocess.call(runCalc2, shell=True)
-
-	return tempraster
+                    # print 'done writing!'
+    return out_vrt
 
 
-def readData(raster):
-	#Load raster data into array
-	print ('loading raster {}\n'.format(raster))
-	filedata = gdal.Open(raster, gdal.GA_ReadOnly).ReadAsArray()
-	return filedata
+def colorize(out_dir, out_file, from_y, to_y):
+    try:
+        # ##--------color_pallette---------------------
+        clr_table = 'color_yolc.txt'
+
+        outcsv_file = f'{out_dir}{os.sep}zzzzzz_{from_y[-2:]}_to_{to_y[-2:]}_list.csv'
+
+        if os.path.isfile(outcsv_file):
+            os.remove(outcsv_file)
+
+        with open(outcsv_file, 'wb') as outcsv2_file:
+            outcsv2_file.write(temp_out2.encode('utf-8') + "\r\n".encode('utf-8'))
+
+        out_VRT = f'{out_dir}{os.sep}zzzz_{from_y[-2:]}-{to_y[-2:]}.vrt'
+
+        com = f'gdalbuildvrt -q -input_file_list {outcsv_file} {out_VRT}'
+        subprocess.call(com, shell=True)
+
+        out_vrt = add_color_table(out_VRT, clr_table, 'UInt16')
+
+        runCom = f'gdal_translate -of GTiff -ot UInt16  -stats -q {out_vrt} {out_file}'
+        subprocess.call(runCom, shell=True)
+
+        # remove the temp files used for adding the color tables
+        for v in glob.glob(out_dir + os.sep + "zzz*"):
+            os.remove(v)
+
+    except:
+        print(traceback.format_exc())
+
+        print("Unexpected error:", sys.exc_info()[0])
 
 
-def allCalc(inDir, outDir, FileFormat, FromY, ToY):
-	try:
-		# create main output directory if it doesn't exist
-		if not os.path.exists(outDir):
-			os.makedirs(outDir)
+def get_files(in_dir):
 
-		FFormat = 'GTiff'
-		exten = '.tif'
-		rastDriver = gdal.GetDriverByName(FFormat) # reading gdal driver
-		rastDriver.Register()
-		
-		RasterList = glob.glob('%s/ChangeMap*%s' %(inDir, exten))
-		RasterList.sort()
-		#del(RasterList[-1:])
-		YearList = []
-		pprint.pprint(RasterList)
-		
-		for x in RasterList:
-			dir, filex = os.path.split(x)
-			Year = (re.split('[ _ .]',filex)[1])[-4:]
-			YearList.append(Year)
-		YearList.sort()
-		pprint.pprint(YearList)
-		#clip year list using from/to years if passed
-		#if not FromY == None and ToY == None:
-		for i in range(0,len(YearList)):
-			try:
-				if int(YearList[0]) < int(FromY):
-					del RasterList[0]
-					del YearList[0]
-				elif int(YearList[-1]) > int(ToY):
-					del RasterList[-1]
-					del YearList[-1]
-			except:
-				IndexError
-		else:
-			FromY, ToY = YearList[0], YearList[-1]
-		
-		###----------calculation_for_year_of_last_change--------------
-		pprint.pprint(RasterList)
-		pprint.pprint(YearList)
-		
-		#final output raster**
-		out_file = '{}{}ccdc{}to{}yolc.tif'.format(outDir, os.sep, FromY[-2:], ToY[-2:])
-		
-		#temp output of change calculations
-		temp_out = '{}{}zzzz{}to{}yolc.tif'.format(outDir, os.sep, FromY[-2:], ToY[-2:])
-		
-		#temp out for color table
-		temp_out2 = '{}{}zzzz2{}to{}yolc.tif'.format(outDir, os.sep, FromY[-2:], ToY[-2:])
-		
-		#Generate the output raster with all zero values for now
-		runCalc0  = '%s/gdal_calc.py --format %s --type %s -A %s --outfile %s --calc="0 *(A > 0)" ' \
-		% (GDALPath, 'GTiff', 'Byte', RasterList[0], temp_out)
-		subprocess.call(runCalc0, shell=True)
-		#-----#
-		p = 1
-	
-		for i in range(len(RasterList)-1, -1, -1):
-			print ('checking image {0}'.format(RasterList[i]))
-			
-			raster1 = RasterList[i]
-			raster1out = outDir + os.sep + 'zzzz_raster1out' + exten
-			
-			# print (YearList[i])
-			
-			proc = '{}{}gdal_calc.py -A {} --outfile {} --format GTiff --type UInt16 --calc="{}*(A > 0)"'.format(GDALPath, os.sep, raster1, raster1out, YearList[i])
-			subprocess.call(proc, shell=True)
-			
-			#temp output of the next process
-			tempraster = '{}{}zzzzz{}.tif'.format(outDir, os.sep, str(i))
-			
-			'''
-			raster1out now is a raster with values 0 or Year.  Compare this with temp_out, which initially is all 0's.
-			For the first time through the loop, any pixel with a Year value is preserved, or is supposed to be.
-			temp_out is then made to equal the output of this test which is tempraster.  This continues for each iteration, 
-			but rather than all 0's, temp_out will now contain Year values for some pixels.  
-			'''
-			
-			if not os.path.exists(tempraster):
-				proc2 = '{}{}gdal_calc.py -A {} -B {} --outfile {} --format GTiff --type UInt16  --calc="(A * (B == 0)) + B"'.format(GDALPath, os.sep, raster1out, temp_out, tempraster)
-				
-				# outfile gets updated with the new sum
-				temp_out = tempraster
-				
-				subprocess.call(proc2, shell=True)
-				
-			p += 1
-			
-		runsubset3 = '%s/gdal_translate -of %s -ot UInt16 -q %s %s ' % (GDALPath, FFormat, tempraster, temp_out2)
-		subprocess.call(runsubset3, shell=True)
-	
-		###--------color_pallette---------------------
-		clr_table = 'color_yolc.txt'
-
-		outcsv_File = r'%s/zzzzzz_%s_to_%s_list.csv' % (outDir, FromY[-2:], ToY[-2:])
-		if os.path.isfile(outcsv_File):
-			os.remove(outcsv_File)
-
-		with open(outcsv_File, 'wb') as outcsv2_File:
-			outcsv2_File.write(temp_out2.encode('utf-8') + "\r\n".encode('utf-8'))
-
-		out_VRT = '{0}{1}zzzz_{2}-{3}.vrt'.format(outDir, os.sep, FromY[-2:], ToY[-2:])
-		com	= '%s/gdalbuildvrt -q -input_file_list %s %s' % (GDALPath, outcsv_File, out_VRT)
-		subprocess.call(com, shell=True)
-
-		out_vrt = add_color_table(out_VRT, clr_table, 'UInt16')
-		runCom  = '%s/gdal_translate -of %s -ot UInt16  -stats -a_srs EPSG:5070 -q -a_nodata 0  %s %s' % (GDALPath, FFormat, out_vrt, out_file)
-		subprocess.call(runCom, shell=True)
-
-		# remove the temp files used for adding the color tables
-		for v in glob.glob(outDir + os.sep+ "zzz*"):
-			os.remove(v)
-			
-	except:
-		print ("Opps! something is not working")
-		print (traceback.format_exc())
-		print  ("Unexpected error:", sys.exc_info()[0])
-
-def usage():
-
-	print('\nUsage:python 5_ccdc_change_accumulation.py\n\n \
-	[-i Full path to the directory containing the CCDC annual change layers] \n\
-	[-frm From Year]\n \
-	[-to To Year]\n \
-	[-o Output Folderpath] \n \
-	[-if input File Format (only 3 format supported: GTiff, HFA, and GRID)] \n \
-	The output raster will be saved in the same format as the input raster.\n\
-	Output raster will be saved in the same format as input raster.\n\n')
+    return sorted(glob.glob(in_dir + os.sep + "*.tif"))
 
 
-def examps():
+def get_years(r_list):
 
-	print('\nUsage:python 5_ccdc_change_accumulation.py -i /some/InputDir -frm 1989 -to 2002 -o /some/OutDir -if GTiff \n')
+    return sorted([re.split('[ _ .]', os.path.split(r_file)[1])[1][-4:] for r_file in r_list])
+
+
+def clip_lists(r_list, y_list, year1, year2):
+    if year1 is None and year2 is None:
+        return r_list, y_list
+
+    if year1 is None:
+        year1 = y_list[0]
+
+    if year2 is None:
+        year2 = y_list[-1]
+
+    for index, year in reversed(list(enumerate(y_list))):
+        if int(year) > int(year2):
+            del y_list[index]
+            del r_list[index]
+        if int(year) < int(year1):
+            del y_list[index]
+            del r_list[index]
+
+    return r_list, y_list, year1, year2
+
+
+def read_data(raster):
+    # Load raster data into array
+    print('loading raster {}\n'.format(raster))
+
+    raster_data = gdal.Open(raster, gdal.GA_ReadOnly).ReadAsArray()
+
+    return raster_data
+
+
+def make_raster(in_data, in_file, out_r):
+    in_src = gdal.Open(in_file, gdal.GA_ReadOnly)
+
+    cols = in_src.RasterXSize
+    rows = in_src.RasterYSize
+
+    driver = gdal.GetDriverByName("GTiff")
+
+    outfile = driver.Create(out_r, cols, rows, 1, gdal.GDT_UInt16)
+
+    if outfile is None:
+        print(f"\nCould not create image file {os.path.basename(out_r)}")
+
+        sys.exit(1)
+
+    outband = outfile.GetRasterBand(1)
+    outband.WriteArray(in_data, 0, 0)
+
+    outband.FlushCache()
+    outband.SetNoDataValue(32767)
+
+    outfile.SetGeoTransform(in_src.GetGeoTransform())
+    outfile.SetProjection(in_src.GetProjection())
+
+    in_src, outfile = None, None
+
+    return None
+
+
+def all_calc_numpy(in_dir, out_dir, year1, year2):
+
+    r_list = get_files(in_dir=in_dir)
+
+    y_list = get_years(r_list=r_list)
+
+    r_list_, y_list_, y1, y2 = clip_lists(r_list, y_list, year1, year2)
+
+    # final output raster**
+    out_file = f'{out_dir}{os.sep}ccdc{y1[-2:]}to{y2[-2:]}yolc.tif'
+
+    # temp output of change calculations
+    temp_out = f'{out_dir}{os.sep}zzzz{y1[-2:]}to{y2[-2:]}yolc.tif'
+
+    # temp out for color table
+    temp_out2 = f'{out_dir}{os.sep}zzzz2{y1[-2:]}to{y2[-2:]}yolc.tif'
+
+    src_0_data = load_data(r_list_[0])
+
+    final_data = np.zeros_like(src_0_data, dtype=np.uint16)
+
+    for file, year in reversed(list(zip(r_list_, y_list_))):
+        raster = load_data(file)
+
+        mask = np.logical_and(raster > 0, final_data == 0)
+
+        final_data[mask == 1] = int(year)
+
+    if np.any(final_data):
+        make_raster(final_data, r_list_[0], out_file)
+
+    else:
+        print(traceback.format_exc())
+
+        print("Resulting array is all zeros:", sys.exc_info()[0])
+
+    colorize(out_dir, out_file, y1, y2)
+
+    return None
 
 
 def main():
-	oFormat, fromY, toY = None, None, None
+    parser = argparse.ArgumentParser()
 
-	argv = sys.argv
+    parser.addargument("-i", "--input", required=True, type=str,
+                       help="Full path to the folder containing CCDC DOY change layers")
 
-	if argv is None:
-		sys.exit(0)
+    parser.addargument("-frm", "-from", "--year1", required=False, type=str, help="The start year")
 
-	# Parse command line arguments.
-	i = 1
-	while i < len(argv):
-		arg = argv[i]
+    parser.addargument("-to", "-to", "--year2", required=False, type=str, help="The end year")
 
-		if arg == '-i':
-			i           = i + 1
-			inputDir        = argv[i]
+    parser.addargument("-o", "--output", required=True, type=str, help="The full path to the output folder")
 
-		elif arg == '-frm':
-			i           = i + 1
-			fromY      = argv[i]
+    args = parser.parse_args()
 
-		elif arg == '-to':
-			i           = i + 1
-			toY      = argv[i]
+    # call the primary function
+    all_calc(args.input, args.output, args.year1, args.year2)
 
-		elif arg == '-o':
-			i           = i + 1
-			outputDir      = argv[i]
-
-		elif arg == '-if':
-			i           = i + 1
-			oFormat      = argv[i]
-
-		elif arg == '-help':
-			usage()
-			sys.exit(1)
-
-		elif arg == '-example':
-			examps()
-			sys.exit(1)
-
-		elif arg[:1] == ':':
-			print('Unrecognized command option: %s' % arg)
-			usage()
-			sys.exit(1)
-
-		i += 1
-	
-	if oFormat == None:
-		oFormat = 'GTiff'
-	
-	# call the primary function	
-	allCalc(inputDir, outputDir, oFormat, fromY, toY)
 
 if __name__ == '__main__':
-	main()
+    main()
 
 t2 = datetime.datetime.now()
-print ("\nCompleted at: ", t2.strftime("%Y-%m-%d %H:%M:%S"))
+print("\nCompleted at: ", t2.strftime("%Y-%m-%d %H:%M:%S"))
 
 tt = t2 - t1
-print ("Processing time: " + str(tt),"\n")
+print("Processing time: " + str(tt), "\n")
