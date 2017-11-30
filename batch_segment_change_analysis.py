1ca96b622b8f038e79b93bb9701a876765334e1f
dzelenak-usgs <daniel.zelenak.ctr@usgs.gov>
2017-11-30 10:36:58 -0600

    added processing time

--- batch_segment_change_analysis.py
+++ batch_segment_change_analysis.py
@@ -8,32 +8,35 @@ Created on Mon Nov 20 15:05:08 2017
 import os
 import argparse
 import subprocess
+import datetime
+
+t1 = datetime.datetime.now()
+print("Processing started at: ", t1.strftime("%Y-%m-%d %H:%M:%S\n"))
 
 
 def main(rootdir, outdir, tile=None, years=None):
-    
     input_list = []
-    
+
     # Get a list of all the tile subfolders in the root input directory
     for root, folders, files in os.walk(rootdir):
         if tile is None:
             for folder in folders:
                 if folder[0] == "h" or folder[0] == "H":
                     input_list.append(os.path.join(root, folder))
-                
+
         else:
             # Otherwise, if a tile was specified only return a list containing that particular tile subfolder
             for folder in folders:
                 if tile in folder:
                     input_list.append(os.path.join(root, folder))
-                    
+
     for f in input_list:
         # Create the output subfolder within the output root directory
         outfolder = outdir + os.sep + os.path.basename(f)
-        
+
         if not os.path.exists(outfolder):
             os.makedirs(outfolder)
-        
+
         # Run with the years argument if it was passed any values
         if years is not None:
             subprocess.call(f"python segment_change_analysis.py -i {f}{os.sep}maps -o {outfolder} -y {years}",
@@ -44,22 +47,27 @@ def main(rootdir, outdir, tile=None, years=None):
             subprocess.call(f"python segment_change_analysis.py -i {f}{os.sep}maps -o {outfolder}", shell=True)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    
+
     parser.add_argument("-i", dest="rootdir", type=str, required=True,
                         help="The full path to the root directory")
-    
-    parser.add_argument("-o" ,dest="outdir", type=str, required=True,
+
+    parser.add_argument("-o", dest="outdir", type=str, required=True,
                         help="The full path to the output root directory")
-    
+
     parser.add_argument("-t", dest="tile", type=str, required=False,
                         help="Optionally specify a tile to process")
-    
+
     parser.add_argument("-y", dest="years", type=str, required=False, default=None, nargs="*",
                         help="Optionally specify one or more years to process")
-    
+
     args = parser.parse_args()
 
     main(**vars(args))
-    
+
+t2 = datetime.datetime.now()
+print("\nCompleted at: ", t2.strftime("%Y-%m-%d %H:%M:%S"))
+
+tt = t2 - t1
+print(f"Processing time: {str(tt)}\n")
